8c5be46ff483edc439cad68121d7f7cab3031096
Mari <marjankhakian@yahoo.com>
2022-09-23 01:55:00 +0330

    Comment cleared in product.js

--- react/src/components/product/product.js
+++ react/src/components/product/product.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export function Product({ data }) {
-  // const data = props.data;
   return (
     <div className="card mt-5">
       <div className="card-body">
